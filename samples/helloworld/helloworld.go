535c87abb195d1d44ab726d2b1fdf69b200b880d
i034796 <tarun.ramakrishna.elankath@sap.com>
2018-05-21 18:40:31 +0530

    WIP: basics ppt

---
+++ samples/helloworld/helloworld.go
@@ -0,0 +1,7 @@
+package main //files with main function must have package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello, 世界")
+}
---
+++ samples/imports/imports.go
@@ -0,0 +1,10 @@
+package main
+
+import ( // "factorized" import statement for multiple imports
+	"fmt"
+	"math"
+)
+
+func main() {
+	fmt.Printf("Now you have %g problems.", math.Sqrt(7))
+}
